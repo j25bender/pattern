e5c19f3c4e420850a7057b721459788d9f526e33
Jeff Bender <j25bender@gmail.com>
2018-03-28 08:57:07 -0600

    tab container

---
+++ script.js
@@ -0,0 +1,28 @@
+const tabContainer = document.getElementById('tab-container');
+const firstTab = document.querySelector('.first, .tab');
+
+firstTab.classList.add('activeTab');
+tabContainer.addEventListener('click', (e) => toggleActiveTabCard(e));
+
+toggleActiveTabCard = (e) => {
+  
+  removeInitialActiveClass();
+  
+  const activeTab = e.target;
+  const activeClass = '.' + activeTab.classList[0];
+  const activeCard = document.querySelectorAll(activeClass)[1].classList;
+  
+  activeCard.add('activeCard');  
+  activeTab.classList.add('activeTab');
+}
+
+removeInitialActiveClass = () => {
+  const allTabs = document.querySelectorAll('.tab');
+  const allCards = document.querySelectorAll('.card');
+  const allTabsLength = Object.keys(allTabs).length;
+  
+  for(let i = 0; i < allTabsLength; i++) {
+    allTabs[i].classList.remove('activeTab');
+    allCards[i].classList.remove('activeCard');
+  }  
+}
